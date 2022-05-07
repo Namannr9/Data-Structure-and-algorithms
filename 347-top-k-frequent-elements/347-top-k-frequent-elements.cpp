93b1840d294bd9cbd35c5d98f598ebdf7da4bd8f
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-05-07 22:24:50 +0530

    Time: 43 ms (5.09%), Space: 13.6 MB (72.27%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    //Note if you want to make size of priority_queue not more then k element
+    // then take min heap
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
-        priority_queue<pair<int,int>> pq;
+   //     priority_queue<pair<int,int>> pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         unordered_map<int,int> freq;
         for(int num:nums) freq[num]++;
         for(auto ele:freq)
         {
             pq.push({ele.second,ele.first});
+            if(pq.size()>k) pq.pop();
         }
         
         vector<int> ans;
@@ -19,26 +23,27 @@ public:
         return ans;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // without using priority_queue
     /*
-    unordered_map<int,int> mp;
-        priority_queue<pair<int,int>> pq;
-        for(auto i:nums) mp[i]++;
-        for(auto ele:mp) pq.push({ele.second,ele.first});
+    static bool comp(pair<int,int> a,pair<int,int> b)
+    {
+        return a.second>b.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        map<int,int> freq;
+        for(int i=0;i<nums.size();i++) freq[nums[i]]++;
+        
+        vector<pair<int,int>> v;
+        for(auto i:freq)
+        {
+            v.push_back({i.first,i.second});
+        }
+        sort(v.begin(),v.end(),comp);
         vector<int> ans;
         for(int i=0;i<k;i++)
         {
-            ans.push_back(pq.top().second);
-            pq.pop();
+            ans.push_back(v[i].first);
         }
         return ans;
     */
