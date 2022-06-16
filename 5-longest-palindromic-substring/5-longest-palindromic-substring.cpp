b0ff9d291207721d8db34ecfbdfed24920fe9c4c
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-16 18:59:13 +0530

    Time: 324 ms (37.76%), Space: 8.4 MB (63.49%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        bool table[n][n];
+        memset(table,0,sizeof(table));
+        int maxlength=1;
+        int start=0;
+        for(int i=0;i<n;i++) table[i][i]=true; //
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                table[i][i+1]=true;
+                start=i;
+                maxlength=2;
+            }
+        }
+        
+        for(int k=3;k<=n;k++)
+        {
+            for(int i=0;i<n-k+1;i++)
+            {
+                int j=i+k-1;
+                if(table[i+1][j-1] && s[i]==s[j])
+                {
+                    table[i][j]=true;
+                    if(k>maxlength)
+                    {
+                        start=i;
+                        maxlength=k;
+                    }
+                }
+            }
+        }
+        
+        string ans="";
+        int end=start+maxlength-1;
+        for(int i=start;i<=end;i++)
+        {
+            ans+=s[i];
+        }
+     
+        return ans;
+        
+    }
+};
