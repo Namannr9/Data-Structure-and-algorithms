4da9426f05e8d31438647e94861803a185321a26
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-21 06:22:13 +0530

    Time: 7 ms (86.38%), Space: 7.3 MB (78.00%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    // hashing
+    bool isAnagram(string s, string t) 
+    {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> count;
+        for(int i=0;i<s.size();i++)
+        {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        
+        for(auto ele : count)
+        {
+            if(ele.second) return false;
+        }
+        return true;
+        
+    }
+    
+    /*
+    sorting
     bool isAnagram(string s, string t) 
     {
         sort(s.begin(),s.end());
@@ -8,4 +29,5 @@ public:
         else return false;
             
     }
+    */
 };
