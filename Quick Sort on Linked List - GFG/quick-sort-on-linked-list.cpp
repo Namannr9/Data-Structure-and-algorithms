ddfc0a1fdac6886f98640f25ba11d5912c322a15
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-23 16:23:01 +0530

    Added solution - LeetHub

---
+++ Quick Sort on Linked List - GFG/quick-sort-on-linked-list.cpp
@@ -0,0 +1,118 @@
+// { Driver Code Starts
+#include <iostream>
+#include <cstdio>
+using namespace std;
+ 
+/* a node of the singly linked list */
+struct node
+{
+    int data;
+    struct node *next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct node* new_node = new node(new_data);
+ 
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+ 
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* A utility function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+void quickSort(struct node **headRef);
+int main()
+{
+	int t,n,x;
+	cin>>t;
+	while(t--){
+		
+        
+        cin>>n;n=n-1;
+		cin>>x;
+        node *temp,*head = new node(x);
+        temp=head;
+        while(n--)
+        {
+			cin>>x;
+          temp->next=new node(x);
+          temp=temp->next;
+			}
+    
+    quickSort(&head);
+ 
+    printList(head);
+ while(head!= NULL){
+     temp=head;
+     head=head->next;
+     free(temp);
+     
+ }
+	}
+    return 0;
+}// } Driver Code Ends
+
+
+ 
+/* a node of the singly linked list 
+struct node
+{
+    int data;
+    struct node *next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; */
+
+//you have to complete this function
+node *partition(node *head,node *tail)
+{
+    int pivot=head->data;
+    node *cur=head;
+    node *put=head;
+    while(cur)
+    {
+        if(cur->data < pivot)
+        {
+            put=put->next;
+            swap(cur->data,put->data);
+        }
+        
+        cur=cur->next;
+    }
+    swap(put->data,head->data);
+    return put;
+}
+node * quick(node *head,node *tail)
+{
+    if(head!=tail)
+    {
+        node *pivot=partition(head,tail);
+        quick(head,pivot);
+        quick(pivot->next,tail);
+    }
+}
+void quickSort(struct node **headRef) 
+{
+    quick(*headRef,NULL);
+}
