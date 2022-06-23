84e07358c66e46bc20b89bb7b4c4d2cc2d87d9f3
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-23 16:43:18 +0530

    Time: 15 ms (43.28%), Space: 11.3 MB (59.92%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -18,29 +18,47 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) 
     {
-        map<Node *,Node *> mp;
-        Node *cur=head;
-        while(cur)
+         if(head==NULL) return NULL;
+        if(head->next==NULL)
         {
-            Node *tmp=new Node(cur->val);
-            mp[cur]=tmp;
-            cur=cur->next;
+            Node *i=new Node(head->val);
+            if(head->random!=NULL) i->random=i;
+            return i;
+        }
+            
+        
+        Node *i=head;
+        Node *front;
+        while(i)
+        {
+            front=i->next;
+            Node *copy=new Node(i->val);
+            i->next=copy;
+            copy->next=front;
+            i=front;
+        }
+        i=head;
+        while(i!=NULL)
+        {
+            if(i->random!=NULL  && i->next!=NULL)
+            {
+                i->next->random=i->random->next;
+            }
+            i=i->next->next;
         }
         
-        Node *dummy=new Node(0);
-        Node *ans=dummy;
-        cur=head;
-        while(cur)
+        i=head;
+        Node *tmp=new Node(0);
+        Node *copy=tmp;
+        while(i)
         {
-            Node *tmp=mp[cur];
-            tmp->next=mp[cur->next];
-            tmp->random=mp[cur->random];
-            
-            ans->next=tmp;
-            ans=ans->next;
-            cur=cur->next;
+            front=i->next->next;
+            copy->next=i->next;
+            i->next=front;
+            copy=copy->next;
+            i=i->next;
         }
-        ans->next=NULL;
-        return dummy->next;
+    return tmp->next;
+    
     }
 };
