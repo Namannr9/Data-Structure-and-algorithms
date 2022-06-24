7e0a318b1931349db31f7d7f00843908eed3e582
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-24 10:23:33 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -8,9 +8,7 @@ class Solution{
   public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
-    // Function to count inversions in the array.
-    
-    long long merge(long long arr[],long long left,long long mid,long long right)
+  long long merge(long long arr[],long long left,long long mid,long long right)
     {
         long long invCount=0;
         long long i=left;
