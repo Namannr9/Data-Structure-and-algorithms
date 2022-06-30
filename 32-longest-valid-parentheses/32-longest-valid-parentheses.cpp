2e44a0a97013029cf9cbf14ebb50248f0a5358c6
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-06-30 10:23:10 +0530

    Time: 12 ms (15.72%), Space: 7.4 MB (38.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-        int n = s.length(), longest = 0;
-        stack<int> st;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') st.push(i);
-            else {
-                if (!st.empty()) {
-                    if (s[st.top()] == '(') st.pop();
-                    else st.push(i);
+    
+    /*
+    in brute force approach we call isValid() for every substring and return maximum length of
+    valid substring
+    */
+    int longestValidParentheses(string s) 
+    {
+        stack<int> stk;
+        stk.push(-1);
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                stk.push(i);
+            }
+            else // )
+            {
+                stk.pop();
+                
+                if(stk.size()==0)
+                {
+                    stk.push(i);
                 }
-                else st.push(i);
+                else
+                {
+                    int len=i-stk.top();
+                    ans=max(ans,len);
+                }
             }
         }
-        if (st.empty()) longest = n;
-        else {
-            int a = n, b = 0;
-            while (!st.empty()) {
-                b = st.top(); st.pop();
-                longest = max(longest, a-b-1);
-                a = b;
-            }
-            longest = max(longest, a);
-        }
-        return longest;
+        return ans;
     }
 };
