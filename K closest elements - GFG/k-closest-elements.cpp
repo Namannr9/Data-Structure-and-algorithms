6b5ed25b67b7080d1b2fd8c085ab9d0b0a8c9b9f
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-01 17:08:36 +0530

    Added solution - LeetHub

--- K closest elements - GFG/k-closest-elements.cpp
+++ K closest elements - GFG/k-closest-elements.cpp
@@ -11,23 +11,52 @@ class Solution{
 public:
     vector<int> printKClosest(vector<int> arr, int n, int k, int x) 
     {
-        priority_queue<pair<int,int>> pq;
+        vector<int>ans;
         
-        vector<int> ans;
-        for(int i=0;i<n;i++)
-        {
-            int dif=abs(x-arr[i]);
-            if(dif!=0) pq.push({dif,-1*arr[i]});
-            if(pq.size() > k) pq.pop();
+        int low=0,high=n-1;
+        int notedindex,mid;
+        while(low<=high){
+          mid=(low+high)/2;
+          
+          if(arr[mid]==x){
+              notedindex=mid;
+            break;
+          }else if(arr[mid]<x){
+              low=mid+1;
+          }else if(arr[mid]>x){
+              high=mid-1;
+          }
         }
-        
-        while(pq.size())
-        {
-            ans.push_back(-1*pq.top().second);
-            pq.pop();
+int i,j;
+        if(arr[mid]==x){
+            i=mid-1;
+            j=mid+1;
+        }else{
+            i=high;
+            j=low;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        //Time =log N +K
+        //Space O(1)
+        int count=0;
+        while(count<k){
+            int left=abs(x-arr[i]);
+            int right=abs(x-arr[j]);
+            if(i<0){
+                ans.push_back(arr[j]);
+                j++;
+            }else if(j>=n){
+                ans.push_back(arr[i]);
+                i--;
+            }
+            else if(left<right && i>=0){
+                ans.push_back(arr[i]);
+                i--;
+            }else if(left>=right && j<n){
+                ans.push_back(arr[j]);
+                j++;
+            } 
+            count++;
+        }return ans;
     }
 };
 
