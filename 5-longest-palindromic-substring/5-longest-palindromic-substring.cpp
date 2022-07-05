88c9e94d47ddc97dc01c560fb7cd26f733c97ef6
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-05 07:04:55 +0530

    Time: 229 ms (51.95%), Space: 246.9 MB (15.21%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
+    string findPalindrome(string s,int left,int right)
+    {
         int n=s.size();
-        bool table[n][n];
-        memset(table,0,sizeof(table));
-        int maxlength=1;
-        int start=0;
-        for(int i=0;i<n;i++) table[i][i]=true; //
-        
+        while(left>=0 && right<n && s[left]==s[right])
+        {
+            left--;
+            right++;
+        }
+        return s.substr(left+1,right-left-1);
+    }
+    string longestPalindrome(string s) 
+    {
+        int n=s.size();
+        if(n<2) return s;
+        string ans="";
+        ans+=s[0];
         for(int i=0;i<n-1;i++)
         {
-            if(s[i]==s[i+1])
-            {
-                table[i][i+1]=true;
-                start=i;
-                maxlength=2;
-            }
+            string odd=findPalindrome(s,i,i);
+            string even=findPalindrome(s,i,i+1);
+            
+            if(odd.size() > ans.size()) ans=odd;
+            if(even.size() > ans.size()) ans=even;
         }
-        
-        for(int k=3;k<=n;k++)
-        {
-            for(int i=0;i<n-k+1;i++)
-            {
-                int j=i+k-1;
-                if(table[i+1][j-1] && s[i]==s[j])
-                {
-                    table[i][j]=true;
-                    if(k>maxlength)
-                    {
-                        start=i;
-                        maxlength=k;
-                    }
-                }
-            }
-        }
-        
-        string ans="";
-        int end=start+maxlength-1;
-        for(int i=start;i<=end;i++)
-        {
-            ans+=s[i];
-        }
-     
         return ans;
-        
     }
 };
