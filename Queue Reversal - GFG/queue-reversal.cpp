632f834cf7bb17339a4df6ec2a5f05e3d9c28f52
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-06 12:58:30 +0530

    Added solution - LeetHub

--- Queue Reversal - GFG/queue-reversal.cpp
+++ Queue Reversal - GFG/queue-reversal.cpp
@@ -32,18 +32,16 @@ int main()
 //function Template for C++
 
 //Function to reverse the queue.
+void solve(queue<int> &q)
+{
+    if(q.empty()) return;
+    int val=q.front();
+    q.pop();
+    solve(q);
+    q.push(val);
+}
 queue<int> rev(queue<int> q)
 {
-    stack<int> st;
-    while(q.size())
-    {
-        st.push(q.front());
-        q.pop();
-    }
-    while(st.size())
-    {
-        q.push(st.top());
-        st.pop();
-    }
+    solve(q);
     return q;
 }
