8a567f010f4a811a97b401cb59db3b174bd789a9
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-15 15:35:33 +0530

    Time: 85 ms (12.07%), Space: 71.5 MB (42.53%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -10,7 +10,62 @@
  */
 class Solution {
 public:
-    ListNode *reverse(ListNode *cur)
+    
+    ListNode *addTwoNode(ListNode *a,ListNode *b,int &carry)
+    {
+        if(a==NULL && b==NULL) return NULL;
+        
+        ListNode *newNode=new ListNode(0);
+        newNode->next=addTwoNode(a->next,b->next,carry);
+        int sum=a->val+b->val+carry;
+        newNode->val=sum%10;
+        carry=sum/10;
+       
+        return newNode;
+    }
+    
+     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+     {
+         
+         // in this loop we ae adding leading zero for making equal size of linked list
+         ListNode *a=l1;
+         ListNode *b=l2;
+         while(a || b)
+         {
+              if(a==NULL)
+              {
+                  ListNode *front=new ListNode(0);
+                  front->next=l1;
+                  l1=front;
+                  b=b->next;
+              }
+              else if(b==NULL)
+              {
+                  ListNode *front=new ListNode(0);
+                  front->next=l2;
+                  l2=front;
+                  a=a->next;
+              }
+              else
+              {
+                  a=a->next;
+                  b=b->next;
+              }
+          }
+         
+         int carry=0;
+         ListNode *dummy=new ListNode(0);
+         dummy->next=addTwoNode(l1,l2,carry);
+         if(carry)
+         {
+             ListNode *front=new ListNode(carry);
+             front->next=dummy->next;
+             dummy->next=front;
+         }
+         return dummy->next;
+     }
+    
+    /*ListNode *reverse(ListNode *cur)
     {
         ListNode *pre=NULL;
         ListNode *next;
@@ -49,4 +104,5 @@ public:
         return reverse(ans->next);
         
     }
+    */
 };
