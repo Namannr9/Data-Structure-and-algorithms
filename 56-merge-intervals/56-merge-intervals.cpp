e6af156b0b26cec4fb7b4ac921d18ba584b803ae
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-16 16:26:15 +0530

    Time: 104 ms (11.29%), Space: 20 MB (26.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-        vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
         vector<int> cur=intervals[0];
         for(auto v:intervals)
         {
             if(v[0]<=cur[1])
             {
-                cur[1]=max(cur[1],v[1]);
+                cur[1]=max(v[1],cur[1]);
             }
             else
             {
