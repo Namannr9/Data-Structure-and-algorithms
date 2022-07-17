0f80fb3990852479da368d5c78913cb327a37873
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-17 16:56:00 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (23.39%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int cnt[3]={0,0,0};
+        for(int i:nums)
+        {
+            cnt[i]++;
+        }
+        int j=0;
+        for(int i=0;i<3;i++)
+        {
+            while(cnt[i]--)
+            {
+                nums[j++]=i;
+            }
+        }
+    }
+};
