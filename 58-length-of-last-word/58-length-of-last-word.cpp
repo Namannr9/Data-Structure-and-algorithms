bd664507cbcb396e9891d90bff9862b9631909ac
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-17 11:09:52 +0530

    Time: 6 ms (19.42%), Space: 6.5 MB (52.03%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int tail=s.size()-1;
+        int len=0;
+        while(tail>=0 && s[tail]==' ') tail--;
+        while(tail>=0 && s[tail]!=' ') 
+        {
+            len++;
+            tail--;
+        }
+        return len;
+        
+    }
+};
