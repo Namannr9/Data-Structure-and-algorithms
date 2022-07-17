7812d592261b1b5bed90ef904a005059d0c8354c
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-17 10:25:02 +0530

    Time: 55 ms (19.43%), Space: 35.7 MB (15.22%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    /*
+    reffrence pepcoding
+    How to conert two bit gray code to three bit grey code:
+    
+    two bit : 00 01 11 10
+    
+    first add "0" in front of string -> 000 001 011 010
+    
+    then add "1" in front of string but at this time start from last code of two bit -> 101 111 101 100
+    
+    */
     vector<string> solve(int n)
     {
         if(n==1) return {"0","1"};
