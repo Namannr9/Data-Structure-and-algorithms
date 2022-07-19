7fa5ed632a10737f33952b8e63621b2a1d7190b1
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-19 16:43:33 +0530

    Time: 84 ms (14.81%), Space: 24.4 MB (31.90%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) 
     {
+        stack<int> st;
+        for(int i=nums.size()-1;i>=0;i--) st.push(nums[i]);
+        vector<int> ans(nums.size());
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums[i]) st.pop();
+            if(st.empty()) ans[i]=-1;
+            else ans[i]=st.top();
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+    /*
+    Time complexcity O(n^2)
+    Space O(1);
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
         int n=nums.size();
         vector<int> ans(n);    
         
@@ -19,4 +36,5 @@ public:
         }
         return ans;
     }
+    */
 };
