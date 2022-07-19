a052d18032329bfac63e55ebee8c857b3dccaa1b
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-19 12:11:35 +0530

    Time: 27 ms (5.80%), Space: 9.6 MB (55.97%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,9 +2,31 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int sum=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if((nums[j]>>i) & 1) sum++;
+            }
+            sum=sum%3;
+            
+            if(sum) // not equal to zero  it means extra one
+            {
+                ans=ans | sum<<i;
+            }
+        }
+        return ans;
+    }
+};    
+    /*
+    int singleNumber(vector<int>& nums) 
+    {
         unordered_map<int,int> mp;
         for(int a:nums) mp[a]++;
         for(auto ele :mp) if(mp[ele.first]==1) return ele.first;
         return -1;
     }
-};
+
+*/
