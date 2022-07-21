fbb3cc11e5a8a47cbd6b331a4f7a08b1eead4b68
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-21 17:43:44 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (16.46%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    // first find dist with left c and then find dist with right c and update with min dist
+    vector<int> shortestToChar(string s, char c) 
+    {
+        int n=s.size();
+        vector<int> dist(n,0);
+        int pos;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]==c) pos=i;
+            dist[i]=i-pos;
+        }
+        
+        for(int i=pos-1;i>=0;i--)
+        {
+            if(s[i]==c) pos=i;
+            dist[i]=min(dist[i],pos-i);
+        }
+        return dist;
+    }
+    /*
+    Time complexcity o(n^2) worst case
+    Space complexcity o(n) worst case
     vector<int> shortestToChar(string s, char c) 
     {
         vector<int> dist;
@@ -18,7 +40,7 @@ public:
             }
             ans.push_back(d);
         }
-        return ans;
-                    
+        return ans;              
     }
+    */
 };
