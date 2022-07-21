e1cb40dfe863d6bff6611912aa5039b8e998c6d1
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-21 22:32:31 +0530

    Time: 2 ms (44.56%), Space: 5.9 MB (64.45%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -2,8 +2,24 @@ class Solution {
 public:
     bool isPerfectSquare(int num) 
     {
+        long long left=1;
+        long long right=num;
+        while(left<=right)
+        {
+            long long mid=left+(right-left)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid < num) left=mid+1;
+            else right=mid-1;
+        }
+        return false;
+    }
+    
+    /*
+    bool isPerfectSquare(int num) 
+    {
         long long i;
         for(i=1;i*i<num;i++);
         return i*i==num;
     }
+    */
 };
