87becd4021cfe71e49313721f1f983178a8be3a0
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-22 11:13:52 +0530

    Time: 6 ms (80.00%), Space: 10 MB (91.93%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        vector<int> ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i1=0;
+        int i2=0;
+        while(i1<n1 && i2<n2)
+        {
+            if(nums1[i1]==nums2[i2])
+            {
+                ans.push_back(nums1[i1]);
+                i1++;
+                i2++;
+            }
+            else if(nums1[i1]>nums2[i2]) i2++;
+            else i1++;
+            if(i1>0) while(i1<n1 && nums1[i1]==nums1[i1-1]) i1++;
+            if(i2>0) while(i2<n2 && nums2[i2]==nums2[i2-1]) i2++;
+        }
+        return ans;
+    }
+    
+    /*
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        unordered_set<int> st(nums1.begin(),nums1.end());
+        vector<int> ans;
+        for(int a:nums2)
+        {
+            if(st.count(a))
+            {
+                ans.push_back(a);
+                st.erase(a);
+            }
+        }
+        return ans;
+    }
+    
     bool search(vector<int> &nums,int target)
     {
         int left=0;
@@ -31,4 +73,5 @@ public:
         }
         return ans;
     }
+    */
 };
