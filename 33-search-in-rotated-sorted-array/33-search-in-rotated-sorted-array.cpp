3223390d11dc44117699a5fc85ff60499e289a10
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-22 16:23:19 +0530

    Time: 6 ms (56.76%), Space: 11.1 MB (29.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        int l=0;
-        int r=nums.size()-1;
-        while(l<=r)
+        int left=0;
+        int right=nums.size()-1;
+        while(left<=right)
         {
-            int mid=(l+r)/2;
+            int mid=left+(right-left)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[l]<=nums[r]) // simple binary search
+            if(nums[left]<=nums[right])        // simple binary search
             {
-                if(target>nums[mid]) l=mid+1;
-                else r=mid-1;
+                if(nums[mid]<target) left=mid+1;
+                else right=mid-1;
             }
-            else if(nums[mid]<=nums[r])
+            else if(nums[mid]<=nums[right])
             {
-                if(target > nums[mid] && target <=nums[r]) l=mid+1;
-                else r=mid-1;
+                if(nums[mid]<target && target<=nums[right]) left=mid+1;
+                else right=mid-1;
             }
             else
             {
-                if(nums[l]<=target && target <nums[mid]) r=mid-1;
-                else l=mid+1;
+                if(nums[left]<=target && target<nums[mid]) right=mid-1;
+                else left=mid+1;
             }
         }
         return -1;
