1d4d448ad7c301779c36ded989c520b1253dc451
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-23 09:26:51 +0530

    Time: 201 ms (5.01%), Space: 59.7 MB (5.79%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& nums) 
     {
+        int left=1;
+        int right=nums.size()-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid-1]>nums[mid]) right=mid-1;
+            else left=mid+1;
+        }
+        return -1;
+    }
+    
+    /*
+    int peakIndexInMountainArray(vector<int>& nums) 
+    {
         int left=0;
         int right=nums.size()-1;
         while(left<right)
@@ -12,4 +27,5 @@ public:
         }
         return left;
     }
+    */
 };
