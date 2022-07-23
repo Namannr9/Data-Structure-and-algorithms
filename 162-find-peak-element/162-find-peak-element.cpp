ab0870aff2b1a03d8092d76cf281c4b251c1e9bc
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-23 09:01:47 +0530

    Time: 4 ms (76.37%), Space: 8.9 MB (49.64%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) 
     {
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[mid+1]) right=mid;
+            else left=mid+1;
+        }
+        return left;
+    }
+    /*
+    int findPeakElement(vector<int>& nums) 
+    {
         int n=nums.size();
         if(n==1 || nums[0]>nums[1]) return 0;
         
@@ -13,4 +26,5 @@ public:
         return -1;
             
     }
+    */
 };
