26db2850381aa4ba26352bdc2f34026fe307f890
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-26 11:05:02 +0530

    Time: 11 ms (98.96%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,15 +9,15 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* a, TreeNode* b) 
+   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        if(root==NULL || a==NULL || b==NULL) return NULL;
-        if(root==a || root==b) return root;
-        TreeNode *left=lowestCommonAncestor(root->left,a,b);
-        TreeNode *right=lowestCommonAncestor(root->right,a,b);
+        if(root==NULL || root==p || root==q) return root;
         
-        if(left && right) return root;
-        if(!left) return right;
-        return left;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        return root;
     }
 };
