e948ba84e30de2ff489ee2d08eca519f082e6e44
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-28 11:37:44 +0530

    Time: 16 ms (72.42%), Space: 21 MB (25.11%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,6 +11,25 @@
  */
 class Solution {
 public:
+    int findDepth(TreeNode *root)
+    {
+        if(root==NULL) return 0;
+        
+        int left=findDepth(root->left);
+        if(left==-1) return -1;
+        int right=findDepth(root->right);
+        if(right==-1) return -1;
+        
+        if(abs(left-right)>1) return -1;
+    
+        return max(left,right)+1;
+        
+    }
+    bool isBalanced(TreeNode* root) 
+    {
+        return findDepth(root)==-1 ? false : true;
+    }
+    /*
     // Time complexity O(n^2);
     int findDepth(TreeNode *root)
     {
@@ -26,4 +45,5 @@ public:
         
         return abs(left-right)<=1 && isBalanced(root->left) && isBalanced(root->right);
     }
+    */
 };
