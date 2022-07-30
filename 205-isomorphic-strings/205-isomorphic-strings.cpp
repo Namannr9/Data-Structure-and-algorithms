404edceccdd80ae961afd7355fbf1f9a3abf76d1
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-30 14:29:52 +0530

    Time: 7 ms (79.28%), Space: 7 MB (74.69%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+     bool isIsomorphic(string s, string t) 
+     {
+        int n=s.size();
+        int m=t.size();
+        if(n!=m) return false;
+         
+         int m1[256]={0};
+         int m2[256]={0};
+         for(int i=0;i<n;i++)
+         {
+             if(m1[s[i]]==0 && m2[t[i]]==0)  // first time visit
+             {
+                 m1[s[i]]=t[i];
+                 m2[t[i]]=s[i];
+             }
+             else if(m1[s[i]]!=t[i]) return false;
+         }
+         return true;
+         
+         
+     }
+    /*
+    // convert both strig to its normal form
     string general(string s)
     {
         unordered_map<char,int> mp;
@@ -21,4 +44,5 @@ public:
         t=general(t);
         return s==t;
     }
+    */
 };
