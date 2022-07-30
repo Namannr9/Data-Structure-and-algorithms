8d8da056ef414956ab0a8dfc148bf8cfeb25e008
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-30 17:34:24 +0530

    Time: 9 ms (19.57%), Space: 6.7 MB (67.80%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
+        int c=0;
+        for(char ch:t) c=c^ch;
+        for(char ch:s) c=c^ch;
+        return c;
+    }
+    /*
+    char findTheDifference(string s, string t) 
+    {
         unordered_map<char,int> mp;
         for(char c:t) mp[c]++;
         for(char c:s) mp[c]--;
         for(auto ele:mp) if(ele.second==1) return ele.first;
         return ' ';
-        
     }
+    */
 };
