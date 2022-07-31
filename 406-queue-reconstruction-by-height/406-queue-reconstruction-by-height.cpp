765c052c702c7519ccb30e9f02cc32fe1d89a928
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-07-31 10:14:45 +0530

    Time: 269 ms (32.01%), Space: 24.6 MB (10.59%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // sort array in descending order 
+    // if two element having same height sort them in ascending order
     static bool cmp(vector<int> a ,vector<int> b)
     {
         if(a[0]==b[0]) return a[1]<b[1];
@@ -14,9 +17,8 @@ public:
         
         for(int i=0;i<people.size();i++)
         {
-            ans.insert(ans.begin()+people[i][1],people[i]);   
+            ans.insert(ans.begin()+people[i][1],people[i]);  // put element in correct position
         }
         return ans;
-        
     }
 };
