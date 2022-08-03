a99de602075484028ec6283708c93ac31abc777d
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-03 13:57:20 +0530

    Time: 127 ms (96.88%), Space: 52.4 MB (82.34%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,5 +1,56 @@
 class MyHashMap {
 private :
+    vector<list<pair<int,int>>> mp;
+    int size;
+public:
+    MyHashMap() 
+    {
+        size=100;
+        mp.resize(size);
+    }
+    
+    int hash(int key)
+    {
+        return key%size;
+    }
+    
+    list<pair<int,int>> :: iterator search(int key)
+    {
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=mp[i].begin();
+        while(it!=mp[i].end())
+        {
+            if(it->first==key) return it;
+            it++;
+        }
+        return it;
+    }
+    void put(int key, int value) 
+    {
+        int i=hash(key);
+        remove(key);
+        mp[i].push_back({key,value});
+    }
+    
+    int get(int key) 
+    {
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=search(key);
+        if(it!=mp[i].end()) return it->second;
+        else return -1;
+    }
+    
+    void remove(int key)
+    {
+        int i=hash(key);
+        list<pair<int,int>>::iterator it=search(key);
+        if(it!=mp[i].end()) mp[i].erase(it);
+    }
+};
+
+
+/*
+private :
     int arr[10000001];
 public:
     MyHashMap() 
@@ -21,12 +72,6 @@ public:
     {
         arr[key]=-1;    
     }
-};
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
+
+*/
