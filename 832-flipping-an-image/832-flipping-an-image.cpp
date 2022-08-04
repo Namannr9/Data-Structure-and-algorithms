1e79f865d24ade91c7deb5a0fec52a088aef7562
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-04 10:08:47 +0530

    Time: 3 ms (95.33%), Space: 8.9 MB (8.94%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -6,6 +6,17 @@ public:
         for(int i=0;i<n;i++)
         {
             reverse(image[i].begin(),image[i].end());
+            for(auto & num : image[i]) num=num^1;
+        }
+        return image;
+    }
+    /*
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) 
+    {
+        int n=image.size();
+        for(int i=0;i<n;i++)
+        {
+            reverse(image[i].begin(),image[i].end());
             for(int j=0;j<n;j++)
             {
                 image[i][j]=(image[i][j]==0) ? 1 : 0;
@@ -13,4 +24,5 @@ public:
         }
         return image;
     }
+    */
 };
