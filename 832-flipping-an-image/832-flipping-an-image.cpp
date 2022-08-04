a9c8a6189584a9ecbad15cf5f8a914cbdff254dc
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-04 10:17:29 +0530

    Time: 12 ms (23.28%), Space: 8.8 MB (75.99%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    
+    /*
+    After row is reversed [1,0,1,1] -> [1,1,0,1]
+    After inversion [1,1,0,1]->[0,0,1,0]
+    
+    if nums are same then just flip them
+    */
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& mat) 
+    {
+        int n=mat.size();
+        for(int row=0;row<n;row++)
+        {
+            int l=0;
+            int r=n-1;
+            while(l<r)
+            {
+                if(mat[row][l]==mat[row][r])
+                {
+                    mat[row][l]^=1;
+                    mat[row][r]^=1;
+                }
+                l++;
+                r--;
+            }
+            if(l==r) mat[row][l]^=1;
+        }
+        return mat;
+    }
+    /*
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) 
     {
         int n=image.size();
@@ -10,7 +39,7 @@ public:
         }
         return image;
     }
-    /*
+    
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) 
     {
         int n=image.size();
