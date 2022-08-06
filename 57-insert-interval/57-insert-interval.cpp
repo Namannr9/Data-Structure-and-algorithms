58733cdf470333e7b4edda94deae03861b72e06a
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-06 13:18:02 +0530

    Time: 20 ms (74.27%), Space: 17.2 MB (38.26%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -2,6 +2,35 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
     {
+        vector<vector<int>> ans;
+        int n=intervals.size();
+        int i=0;
+        while(i<n && intervals[i][1]<newInterval[0])
+        {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        
+        while(i<n && intervals[i][0]<=newInterval[1])
+        {
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        
+        while(i<n) ans.push_back(intervals[i++]);
+        
+        return ans;
+        
+        
+    }
+};
+
+/*
+    insert newInterval in old intervals and again sort them and using mergeinterval strategy solve it.
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
         intervals.push_back(newInterval);
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
@@ -21,4 +50,4 @@ public:
         ans.push_back(cur);
         return ans;
     }
-};
+*/
