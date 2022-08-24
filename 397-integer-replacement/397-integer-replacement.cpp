99643dc15e30f9e2cc797f93ae153305fb9b1e4a
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-24 11:50:46 +0530

    Time: 6 ms (32.07%), Space: 6 MB (53.08%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int integerReplacement(int n) 
+    {
+        if(n==1) return 0;
+        
+        if(n%2==0) return 1+integerReplacement(n/2);
+        if(n==INT_MAX) return integerReplacement(n-1);
+        return min(integerReplacement(n-1),integerReplacement(n+1))+1;
+    }
+};
