dbd8d32e14514058cd26d245aaf8d9ffbd8ceaa0
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-24 06:59:15 +0530

    Time: 4 ms (41.32%), Space: 6 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,6 +3,13 @@ public:
     bool isPowerOfTwo(int n) 
     {
         if(n==0) return false;
+        if(n==1) return true;
+        return n%2==0 && isPowerOfTwo(n/2);
+    }
+    /*
+    bool isPowerOfTwo(int n) 
+    {
+        if(n==0) return false;
         while(n!=1)
         {
             if(n%2) return false;
@@ -10,4 +17,5 @@ public:
         }
         return true;
     }
+    */
 };
