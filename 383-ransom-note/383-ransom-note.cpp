043fd2b73407bda8f4a016446c236a7737ccc760
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-25 07:12:10 +0530

    Time: 29 ms (45.16%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) 
     {
-        unordered_map<char,int> mp;
         unordered_map<char,int> fre;
-        for(char c:ransomNote)
-        {
-            mp[c]++;
-        }
         
         for(char c:magazine)
         {
             fre[c]++;
         }
         
-        for(auto ele:mp)
+        for(char c:ransomNote)
         {
-            if(fre[ele.first]<ele.second) return false;
+            if(fre[c]>0) fre[c]--;
+            else return false;
         }
         return true;
     }
