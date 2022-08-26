977d47cf564898638f4ace7d3778290ef5d472b7
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-26 09:25:00 +0530

    Time: 11 ms (46.86%), Space: 9.3 MB (10.88%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -3,9 +3,13 @@ public:
     int largestSumAfterKNegations(vector<int>& nums, int k) 
     {
         priority_queue<int,vector<int>,greater<int>> pq;
-        
-        for(int a:nums) pq.push(a);
-        
+    
+        int sum=0;
+        for(int num : nums)
+        {
+            pq.push(num);
+            sum+=num;
+        }
         while(k)
         {
             int a=pq.top();
@@ -13,18 +17,16 @@ public:
             else 
             {
                 pq.pop();
+                sum-=a;
                 a=-a;
                 k--;
                 pq.push(a);
+                sum+=a;
             }
             
         }
-        int sum=0;
-        while(pq.size())
-        {
-            sum+=pq.top();
-            pq.pop();
-        }
+        
+
         return sum;
         
         
