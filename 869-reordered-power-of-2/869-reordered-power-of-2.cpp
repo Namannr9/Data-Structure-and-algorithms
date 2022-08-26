dca0c8a819c3d9cb1a456713ba7136af995ffc98
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-26 21:49:23 +0530

    Time: 3 ms (73.16%), Space: 6.4 MB (36.72%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -2,6 +2,26 @@ class Solution {
 public:
     bool reorderedPowerOf2(int n) 
     {
+        vector<string> powers;
+        
+        for(int i=0;i<31;i++)
+        {
+            int p=pow(2,i);
+            string tmp=to_string(p);
+            
+            sort(tmp.begin(),tmp.end());
+            //cout<<tmp<<endl;
+            powers.push_back(tmp);
+        }
+        
+        string find=to_string(n);
+        sort(find.begin(),find.end());
+        for(string s:powers) if(s==find) return true;
+        return false;
+    }
+    /*
+    bool reorderedPowerOf2(int n) 
+    {
         unordered_map<int,vector<string>> mp;
         long long int num=1;
         for(int i=1;i<32;i++)
@@ -20,4 +40,5 @@ public:
         }
         return false;
     }
+    */
 };
