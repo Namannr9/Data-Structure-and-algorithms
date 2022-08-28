836398db966a226bb33db8b61e51d18263d88cdc
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-28 15:37:35 +0530

    Time: 12 ms (72.63%), Space: 7.1 MB (51.32%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int characterReplacement(string s, int k) 
     {
-        int l=0;
-        int r=0;
-        int same=0;
         unordered_map<char,int> mp;
         int window=0;
+        int same=0;
+        int l=0;
+        int r=0;
         int ans=0;
         while(r<s.size())
         {
@@ -19,8 +19,10 @@ public:
                 l++;
             }
             ans=max(ans,r-l+1);
+            
             r++;
         }
         return ans;
+        
     }
 };
