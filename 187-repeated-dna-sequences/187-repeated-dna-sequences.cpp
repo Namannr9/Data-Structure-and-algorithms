9b2533ca765d0943e6c5703afb30f9737bd3a8b3
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-28 13:00:24 +0530

    Time: 122 ms (38.24%), Space: 23.4 MB (50.49%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) 
     {
-        unordered_map<string,int> mp;
-        if(s.size()<=10) return {};
-        for(int i=0;i<s.size()-9;i++)
+        unordered_map<string,int> fre;
+        
+        for(int i=0;i<s.size();i++)
         {
             string tmp=s.substr(i,10);
-            mp[tmp]++;
+            fre[tmp]++;
         }
         vector<string> ans;
-        for(auto ele : mp)
+        
+        for(auto ele : fre)
         {
             if(ele.second>1) ans.push_back(ele.first);
         }
+        
         return ans;
+          
             
-    }
+   }
 };
