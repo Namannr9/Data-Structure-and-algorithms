cd5bb2efe3dc3662d5d8b090b28d3698fcbe153b
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-08-28 12:14:28 +0530

    Time: 21 ms (65.98%), Space: 8.3 MB (75.49%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,17 @@ public:
     {
         unordered_map<char,int> mp;
         int l=0;
-        int len=0;
-        for(int r=0;r<s.size();r++)
+        int r=0;
+        int ans=0;
+        for(;r<s.size();r++)
         {
             if(mp.find(s[r])!=mp.end())
             {
                 l=max(l,mp[s[r]]+1);
             }
-            
-            len=max(len,(r-l+1));
+            ans=max(ans,r-l+1);
             mp[s[r]]=r;
         }
-        return len;
+        return ans;
     }
-};
+};  
