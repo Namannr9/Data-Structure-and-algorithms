838105e8b87c130f6f4c12c8e3005b7fafff1072
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-09-23 16:27:42 +0530

    Time: 349 ms (78.54%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -5,6 +5,11 @@ set N=N-1;
       
       Select distinct salary from Employee order by salary desc
       limit 1 offset N
+      /*
+        SELECT DISTINCT Salary
+        FROM Employee e1
+        WHERE N-1 = (SELECT COUNT(DISTINCT Salary) FROM Employee e2 WHERE e1.Salary <               e2.Salary)
+      */
       
   );
 END
