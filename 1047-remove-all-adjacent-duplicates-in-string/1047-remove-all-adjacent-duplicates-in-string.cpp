9f69c3398b7febf6e5bdc92655fd9553c1447951
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-10-04 15:10:06 +0530

    Time: 1207 ms (5.38%), Space: 780.7 MB (6.57%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     string removeDuplicates(string s) 
     {
+        stack<char> stk;
+        
+        for(char c:s)
+        {
+            if(stk.empty()) stk.push(c);
+            else if(stk.top()==c) stk.pop();
+            else stk.push(c);
+        }
+        
+        string ans="";
+        while(stk.size())
+        {
+            ans=stk.top()+ans;
+            stk.pop();
+        }
+        return ans;
+    }
+};
+/*
+ string removeDuplicates(string s) 
+    {
          string ans;
         for(char c: s)
         {
@@ -11,4 +32,4 @@ public:
         }
         return ans;
     }
-};
+*/
