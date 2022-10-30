4cc7e7a51bacf60b1c42a540457f43849a474269
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-10-30 16:48:05 +0530

    Time: 239 ms (66.96%), Space: 48.7 MB (20.17%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    /* this function will return number of subarray having atmost k diffrent interger 
+    
+    but we want to calculate the number of subarray which having exctally k diffrent integer 
+    so what we are doing ..?
+    
+    we just calculate subarray having atmost k diffrent integer and substarct number of subarray 
+    having at most k-1 diffrent integer 
+    
+    In this way we get the exact number of subarray having exactlly k diffrent integer
+    
+    */
+    
+
     int solve(vector<int> nums,int k)
     {
         unordered_map<int,int> mp;
