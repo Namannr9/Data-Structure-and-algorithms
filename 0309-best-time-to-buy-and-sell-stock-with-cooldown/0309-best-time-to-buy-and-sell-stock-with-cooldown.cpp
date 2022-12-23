dd2ea0678cb36dfc72d518d8984b249155938026
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2022-12-23 22:59:15 +0530

    Time: 95 ms (9.02%), Space: 326.7 MB (5.04%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    map<vector<int>, int>mp;// dp.
-    int util(vector<int>& arr, int idx, int buy, int isCoolDown){
-        if(idx==arr.size()){
-            return 0;
+    int solve(int index,int buy,int n,vector<int> prices,vector<vector<int>> &dp)
+    {
+        if(index>=n) return 0;
+        
+        if(dp[index][buy]!=-1) return dp[index][buy];
+        //int profit=0;
+        if(buy)
+        {
+             return dp[index][buy]=max(solve(index+1,0,n,prices,dp)-prices[index],solve(index+1,1,n,prices,dp));
         }
-        if(mp.find({idx, buy, isCoolDown})!=mp.end())return mp[{idx, buy, isCoolDown}];
-        if(buy==1){//buying phase
-            if(isCoolDown==0){//0=>not a cooling day.
-                return mp[{idx, buy, isCoolDown}]=max(util(arr, idx+1, 0, 0)-arr[idx], util(arr, idx+1, 1,0));
-            }else{//cooling day.
-                return mp[{idx, buy, isCoolDown}]=util(arr, idx+1, buy, 0);
-            }
-        }else{//selling phase.
-            return mp[{idx, buy, isCoolDown}]=max(util(arr, idx+1, 1, 1)+arr[idx], util(arr, idx+1, 0, 0));
+        else
+        {
+            return dp[index][buy]=max(solve(index+2,1,n,prices,dp)+prices[index],
+                             solve(index+1,0,n,prices,dp));
         }
     }
-    int maxProfit(vector<int>& prices) {
-        return util(prices, 0, 1, 0);
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(0,1,n,prices,dp);
     }
 };
