5e554c48964d1ac076ed49e01a703b5395760225
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2023-01-14 07:34:11 +0530

    Time: 22 ms (92.27%), Space: 13.7 MB (92.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int find(int node ,vector<int> &par)
+    int find(int node ,vector<int> &par)   // find the parent of node
     {
         if(node==par[node]) return node;
         return par[node]=find(par[node],par);
     }
+    
+    
     int findCircleNum(vector<vector<int>>& isConnected) 
     {
         int n=isConnected.size();
         
-        int group=n;
+        int group=n;   // assuming there are total n groups ans no cities are connected
         
         vector<int> par(n);
         
@@ -19,15 +21,15 @@ public:
         {
             for(int j=i+1;j<n;j++)
             {
-                if(isConnected[i][j])
+                if(isConnected[i][j])           // if cities are connected 
                 {
                     int x=find(i,par);
                     int y=find(j,par);
                     
-                    if(x!=y)
+                    if(x!=y)        // if there parent are not same they are not belong to same group 
                     {
-                        par[x]=y;
-                        group--;
+                        par[x]=y;         // make there group same 
+                        group--;          // merging group will decrement our count
                     }
                 }
             }
