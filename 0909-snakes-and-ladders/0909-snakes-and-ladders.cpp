b7b2b41adf072259b9515a6ac09dd2386f3fef3c
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2023-01-24 11:15:36 +0530

    Time: 27 ms (62.23%), Space: 12.8 MB (70.82%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    pair<int,int> findCordinate(int val,int n)
+    {
+        int row = n - (val - 1) / n - 1;
+        int col = (val - 1) % n;
+        if (row % 2 == n % 2) {
+            return {row, n - 1 - col};
+        } else {
+            return {row, col};
+        }
+        // int row=(val-1)/n;
+        // int col=(val%n)-1;
+        // if(row%2==0) // even
+        // {
+        //     return {row,col};
+        // }
+        // else
+        // {
+        //     return {row,n-col-1};
+        // }
+    }
+    int snakesAndLadders(vector<vector<int>>& board) 
+    {
+        int n=board.size();
+        int step=0;
+        int target=n*n;
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        vis[n-1][0]=true;
+        queue<int> q;
+        q.push(1);
+        while(q.size())
+        {
+            
+            int sz=q.size();
+            while(sz--)
+            {
+                int cur=q.front();
+                q.pop();
+
+                if(cur==target) return step;
+                for(int i=1;i<=6;i++)
+                {
+                    if(cur+i > target) break;
+                    pair<int,int> p=findCordinate(cur+i,n);
+                    int row=p.first;
+                    int col=p.second;
+
+                    if(vis[row][col]==false)
+                    {
+                        vis[row][col]=true;
+                        if(board[row][col]==-1) q.push(cur+i);
+                        else q.push(board[row][col]);
+                    }
+
+                }
+            }
+            step++;
+        }
+        
+        return -1;
+    }
+};
