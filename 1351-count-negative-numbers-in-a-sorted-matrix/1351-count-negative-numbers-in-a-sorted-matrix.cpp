a8869b50e5d9a3c4b023af818372f4e8a01cb9c7
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2023-06-08 17:38:45 +0530

    Time: 15 ms (82.04%), Space: 10.4 MB (31.99%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int> &nums)
-    {
-        int n=nums.size();
-        int left=0;
-        int right=n-1;
-        
-        while(left<=right)      // think   4 3 2 1 -1 -2 -3
-        {
-            int mid=(left+right)/2;
-            if(nums[mid]>=0) left=mid+1;
-            else right=mid-1;
-        }
-        return n-right-1;
-    }
     int countNegatives(vector<vector<int>>& grid) 
     {
-        int ans=0;
+        int count=0;
         for(int i=0;i<grid.size();i++)
         {
-            ans+=solve(grid[i]);
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]<0) count++;
+               // cout<<grid[i][j]<<endl;
+            }
         }
-        return ans;
+        return count;
     }
+//     int solve(vector<int> &nums)
+//     {
+//         int n=nums.size();
+//         int left=0;
+//         int right=n-1;
+        
+//         while(left<=right)      // think   4 3 2 1 -1 -2 -3
+//         {
+//             int mid=(left+right)/2;
+//             if(nums[mid]>=0) left=mid+1;
+//             else right=mid-1;
+//         }
+//         return n-right-1;
+//     }
+//     int countNegatives(vector<vector<int>>& grid) 
+//     {
+//         int ans=0;
+//         for(int i=0;i<grid.size();i++)
+//         {
+//             ans+=solve(grid[i]);
+//         }
+//         return ans;
+//     }
 };
