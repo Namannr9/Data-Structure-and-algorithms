2b42b8585d9c4bb874838b593111a6e7150691ac
Naman Rathod <96347009+Namannr9@users.noreply.github.com>
2023-08-13 11:00:12 +0530

    Time: 2 ms (46.65%), Space: 8.4 MB (74.29%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -10,4 +10,11 @@ public:
         }
         return count<=1;
     }
+    /*
+    how this logic works
+    
+    count variable maintain how many time array break the violation of ascending order if it is 0 that 
+    means array is in sorted order or if it is 1 then it mean it is roated by some place 
+    but if it is more than 1 than array is not present in sorted order.
+    */
 };
